0fd1d48e02d651da095a947549a5e93b8dbe1342
Nain Lobato Garcia <wolverinenain@gmail.com>
2018-01-16 12:30:00 -0600

    Modelos de usuario, paciente y catEspecialidad

--- app/User.php
+++ app/User.php
@@ -9,13 +9,15 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'idUsuario', 'nomUsuario', 'nombre', 'idEspecialidad', 'email', 'password', 'nivel'
     ];
 
     /**
@@ -26,4 +28,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function especialidades(){
+        return $this->hasMany('App\Models\CatEspecialidad');
+    }
 }
